ddee35bc2049b6b98bc5a3a30414a94f0900c40b
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-09-11 21:05:12 -0400

    added bootstrap and variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-alert('hello world');
+var searchTerm = $(this).attr('data-name');
+var NumberRecords 
+var startYear 
+var endYear
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&fq=pub_year:(%22"+ startYear + "%22)&api-key=8tetUGeUgnAa4G0tymczZyLkjlNSUTDx";
